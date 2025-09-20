4578588bcd899c35f8ac94a405ae02a55f252ee8
diako <diako4048@gmail.com>
2025-09-20 12:30:30 +0330

    fix(conflict):resolve conflicts 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ import Utilisateurs from "./pages/Utilisateurs"
 import Coming from "./pages/Coming"
 import SignUp from "./pages/SignUp"
 import ForgettonPassword from "./pages/ForgettonPassword"
+import Layout from "./component/partial/Layout/Layout"
+import Edit from "./component/ui/organism/edit/Edit"
 import EditItemContext from "./context/EditItemContext"
 import { AuthProvider } from "./context/AuthContext"
 import Homepage from "./pages/Homepage"
 import ProtectedRout from "./pages/ProtectedRout"
-// import Layout from "./component/partial/Layout/Layout"
 
 function App() {
 
@@ -18,30 +19,32 @@ function App() {
     <AuthProvider>
       <EditItemContext>
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<ProtectedRout role={'user'}>
-              <Homepage />
-            </ProtectedRout>} />
-            <Route path="/login" element={<Loging />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/forgettonpassword" element={<ForgettonPassword />} />
+          <Layout>
+            <Routes>
+              <Route path="/" element={<ProtectedRout role={'user'}>
+                <Homepage />
+              </ProtectedRout>} />
+              <Route path="/login" element={<Loging />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/forgettonpassword" element={<ForgettonPassword />} />
 
-            <Route path={`/utilisateurs`} element={<ProtectedRout role={'admin'}>
-              <Utilisateurs />
-            </ProtectedRout>} />
+              <Route path={`/utilisateurs`} element={<ProtectedRout role={'admin'}>
+                <Utilisateurs />
+              </ProtectedRout>} />
 
-            <Route path={'/controle'} element={<ProtectedRout role={'admin'}>
-              <Controle />
-            </ProtectedRout>} />
+              <Route path={'/controle'} element={<ProtectedRout role={'admin'}>
+                <Controle />
+              </ProtectedRout>} />
 
-            <Route path={'/coming'} element={<ProtectedRout role={'admin'}>
-              <Coming />
-            </ProtectedRout>} />
-            
-            <Route path={'/base'} element={<ProtectedRout role={'admin'}>
-              <Base />
-            </ProtectedRout>} />
-          </Routes>
+              <Route path={'/coming'} element={<ProtectedRout role={'admin'}>
+                <Coming />
+              </ProtectedRout>} />
+
+              <Route path={'/base'} element={<ProtectedRout role={'admin'}>
+                <Base />
+              </ProtectedRout>} />
+            </Routes>
+          </Layout>
         </BrowserRouter>
       </EditItemContext>
     </AuthProvider>
--- src/pages/Coming.jsx
+++ src/pages/Coming.jsx
@@ -1,10 +1,7 @@
-import SideBar from "./SideBar";
-
 export default function Coming(){
 
     return(
         <>
-        {/* <SideBar /> */}
         </>
     )
 }
